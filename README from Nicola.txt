259c1391325b1c904683256c966ee4c223fea47b
Nicola <nneecola@gmail.com>
2022-01-27 15:11:41 -0500

    Now it gets coordinates of the center of the ball

--- README from Nicola.txt
+++
@@ -1,5 +0,0 @@
-Hey guys, last night I finished some stuff that I started during practice yesterday.
-I organized the code so that now we only have 1 file for the ball recognition.
-
-Now the repository is made of the main file, the ball processing file, the tape recognition file and
-a settings file (the file where we store what team we are and the settings for the thresholding for both blue and red balls)
--- main.py
+++ main.py
@@ -2,34 +2,47 @@ import cv2
 import numpy
 import math
 from enum import Enum
+import datetime
 from ball_processing import BallProcessing
 import tape_settings
 from general_settings import team
 
+
 if __name__ == "__main__":
-    
+
     settings = BallProcessing(team)
 
     while True:
-        
+        # for timing the process
+        #begin_time = datetime.datetime.now()
 
-        #capturing balls and getting the contours
+        # capturing balls and getting the contours
         video = cv2.VideoCapture(0)
+        
+        
         _, video = video.read()
+
         ball = settings.process(video)
         
+
         M = cv2.moments(ball)
 
-        cv2.imshow("Video", video)
-        cv2.imshow("Detected Ball", ball)
+        #print(M)
+        #cv2.imshow("Video", video)
+        #cv2.imshow("Detected Ball", ball)
 
-        #finding center of the ball (just copy-pasted, don't know what it actually does)
-        #cX = int(M["m10"] / M["m00"])
-        #cY = int(M["m01"] / M["m00"])
 
-        #print(f'Coordinates of the center: ({cX},{cY})')
+        # finding center of the ball (just copy-pasted, don't know what it actually does)
+        cX = int(M['m10'] / M['m00'])
+        cY = int(M['m01'] / M['m00'])
 
+        print(f'Coordinates of the center: ({cX},{cY})')
 
         #cv2.imshow("Source with contours", image)
         #cv2.imshow("Contours", contours)
+
+
+        # for timing the process
+        #print(datetime.datetime.now() - begin_time)
+
         cv2.waitKey(5)
