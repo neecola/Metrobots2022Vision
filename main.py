dc8a6c6cc631411271b67affb7a2fbada39c96eb
Nicola <nneecola@gmail.com>
2022-02-04 12:25:12 -0500

    Added first steps in communication of data with NetworkTables + code cleaning

--- main.py
+++ main.py
@@ -6,11 +6,12 @@ import datetime
 from ball_processing import BallProcessing
 import general_settings as g_sets
 from tape_processing import TapeProcessing
-    
+from networktables import NetworkTables
+
 
 if __name__ == "__main__":
 
-    settings = BallProcessing(g_sets.team)
+    settings = BallProcessing(g_sets.TEAM)
 
     while True:
         # for timing the process #
@@ -18,13 +19,18 @@ if __name__ == "__main__":
 
         # getting the video signal from the camera #
         video = cv2.VideoCapture(0)
+        print(video.set(cv2.CAP_PROP_EXPOSURE,1))
+        
+        
+        
         _, frame = video.read()
+        
         
         # using picture as video signal for testing #
         #frame = cv2.imread("2022VisionSampleImages/NearLaunchpad5ft4in.png")
         
         frame = cv2.resize(
-            frame, (g_sets.frame_size_width, g_sets.frame_size_height))
+            frame, (g_sets.FRAME_SIZE_WIDTH, g_sets.FRAME_SIZE_HEIGHT))
 
         ball = settings.process(frame)
 
@@ -32,15 +38,16 @@ if __name__ == "__main__":
         # finding center of the ball #
         M = cv2.moments(ball)
         try:
-            cX = int(M['m10'] / M['m00']) - (g_sets.frame_size_width/2)
+            cX = int(M['m10'] / M['m00']) - (g_sets.FRAME_SIZE_WIDTH/2)
             cY = abs(int(M['m01'] / M['m00']) -
-                     g_sets.frame_size_height) - (g_sets.frame_size_height / 2)
-            ctr_coords = (cX, cY)
+                     g_sets.FRAME_SIZE_HEIGHT) - (g_sets.FRAME_SIZE_HEIGHT / 2)
+            ball_coords = (cX, cY)
         except ZeroDivisionError:
-            ctr_coords = None
+            ball_coords = None
 
-        #print(ctr_coords)
+        #print(ball_coords)
 
+        #ball_x, ball_y = ball_coords
 
         # now tape #
         tape = TapeProcessing().process(frame)       
@@ -50,9 +57,21 @@ if __name__ == "__main__":
             tape_coords, _, _ = rect
         except :
             tape_coords = None
-        print(tape_coords)
-        print(tape_coords)
         
+        #print(tape_coords)
+        
+        #tape_x, tape_y = tape_coords
+
+
+        NetworkTables.initialize(server=g_sets.ROBO_RIO_ADDRESS)
+        sd = NetworkTables.getTable('SmartDashboard')
+
+        #sd.putNumber('ball_x', ball_x)
+        #sd.putNumber('ball_y', ball_y)
+        #sd.putNumber('tape_x', tape_x)
+        #sd.putNumber('tape_y', tape_y)
+
+
         # for timing the process #
         #print(datetime.datetime.now() - begin_time)
 
